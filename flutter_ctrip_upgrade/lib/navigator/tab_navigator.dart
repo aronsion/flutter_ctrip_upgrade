a426e93d377a04d178e6225eea5ceef1c026638e
Cappuccino <arons.hui@gmail.com>
2020-09-04 09:58:01 +0800

    项目搭建
    项目架构完成

---
+++ flutter_ctrip_upgrade/lib/navigator/tab_navigator.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:upgrade/pages/destination_page.dart';
+import 'package:upgrade/pages/home_page.dart';
+import 'package:upgrade/pages/my_page.dart';
+import 'package:upgrade/pages/travel_page.dart';
+
+class TabNavigator extends StatefulWidget {
+  @override
+  _TabNavigatorState createState() => _TabNavigatorState();
+}
+
+class _TabNavigatorState extends State<TabNavigator> {
+  final _defaultColor = Color(0xff8a8a8a);
+  final _activeColor = Color(0xff50b4ed);
+
+  int _currentIndex = 0;
+
+  final PageController _controller = PageController(
+    initialPage: 0,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        controller: _controller,
+        children: [HomePage(), DestinationPage(), TravelPage(), MyPage()],
+        physics: NeverScrollableScrollPhysics(), //禁止滑动
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          _controller.jumpToPage(index);
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(
+              icon: Image.asset(
+                'images/xiecheng.png',
+                width: 22.0,
+                height: 22.0,
+              ),
+              activeIcon: Image.asset('images/xiecheng_active.png',
+                  width: 22.0, height: 22.0),
+              title: Text(
+                '首页',
+                style: TextStyle(
+                    color: _currentIndex != 0 ? _defaultColor : _activeColor),
+              )),
+          BottomNavigationBarItem(
+              icon: Image.asset(
+                'images/mude.png',
+                width: 24.0,
+                height: 24.0,
+              ),
+              activeIcon: Image.asset(
+                'images/mude_active.png',
+                width: 24.0,
+                height: 24.0,
+              ),
+              title: Text(
+                '目的地',
+                style: TextStyle(
+                    color: _currentIndex != 1 ? _defaultColor : _activeColor),
+              )),
+          BottomNavigationBarItem(
+              icon: Image.asset(
+                'images/lvpai.png',
+                width: 23.0,
+                height: 23.0,
+              ),
+              activeIcon: Image.asset(
+                'images/lvpai_active.png',
+                width: 23.0,
+                height: 23.0,
+              ),
+              title: Text(
+                '旅拍',
+                style: TextStyle(
+                    color: _currentIndex != 2 ? _defaultColor : _activeColor),
+              )),
+          BottomNavigationBarItem(
+              icon: Image.asset(
+                'images/wode.png',
+                width: 23.0,
+                height: 23.0,
+              ),
+              activeIcon: Image.asset(
+                'images/wode_active.png',
+                width: 23.0,
+                height: 23.0,
+              ),
+              title: Text(
+                '我的',
+                style: TextStyle(
+                    color: _currentIndex != 3 ? _defaultColor : _activeColor),
+              )),
+        ],
+      ),
+    );
+  }
+}
---
+++ flutter_ctrip_upgrade/lib/pages/destination_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class DestinationPage extends StatefulWidget {
+  @override
+  _DestinationPageState createState() => _DestinationPageState();
+}
+
+class _DestinationPageState extends State<DestinationPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text('目的地'),
+      ),
+    );
+  }
+}
