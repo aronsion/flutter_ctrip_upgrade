927f5f397b662962f29ea1630db3ff054c78a7ce
Cappuccino <arons.hui@gmail.com>
2020-09-04 15:29:32 +0800

    首页页面、网络请求

--- flutter_ctrip_upgrade/lib/pages/home_page.dart
+++ flutter_ctrip_upgrade/lib/pages/home_page.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:upgrade/model/common_model.dart';
+import 'package:upgrade/model/grid_nav_model.dart';
+import 'package:upgrade/model/sales_box_model.dart';
+import 'package:upgrade/widget/loading_container.dart';
+
+const AppBAR_SCROLL_OFFSET = 100;
+const SEARCH_BAR_DEFAULT_REXT = "目的地 ｜ 酒店｜景点｜航班号";
 
 class HomePage extends StatefulWidget {
   @override
@@ -6,12 +13,68 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  bool _isLoading = false;
+  double appbarAlpha = 0;
+  List<CommonModel> localNavList = [];
+  GridNavModel gridNavModel;
+  List<CommonModel> subNavList = [];
+  SalesBoxModel salesBoxModel;
+
+  List<CommonModel> bannerList = [];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _handleRefresh();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text('首页'),
+    return Scaffold(
+      backgroundColor: Color(0xfffafafc),
+      body: LoadingContainer(
+        isLoading: _isLoading,
+        child: Stack(
+          children: [
+            MediaQuery.removeViewPadding(
+                context: context,
+                removeTop: true,
+                child: NotificationListener(
+                  onNotification: (scrollNotification) {
+                    if (scrollNotification is ScrollUpdateNotification &&
+                        scrollNotification.depth == 0) {
+                      _onScroll(scrollNotification.metrics.pixels);
+                    }
+                    return true;
+                  },
+                  child: RefreshIndicator(
+                    onRefresh: _handleRefresh,
+                  ),
+                ))
+          ],
+        ),
       ),
     );
+  }
+
+  void _onScroll(offset) {
+    double alpha = offset / AppBAR_SCROLL_OFFSET;
+    if (alpha < 0) {
+      alpha = 0;
+    } else if (alpha > 1) {
+      alpha = 1;
+    } else {}
+    setState(() {
+      appbarAlpha = alpha;
+    });
+    print(alpha);
+  }
+
+  /**
+   * 网络请求数据
+   */
+  Future<Null> _handleRefresh() async {
+    try {} catch (e) {}
   }
 }
