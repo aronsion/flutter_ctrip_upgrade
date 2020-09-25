2508e49d708969a41061a74c58a332cd0a9e1584
Cappuccino <arons.hui@gmail.com>
2020-09-25 11:28:09 +0800

    android集成百度语音插件

--- flutter_ctrip_upgrade/android/app/src/main/kotlin/com/ctrip/upgrade/MainActivity.kt
+++ flutter_ctrip_upgrade/android/app/src/main/kotlin/com/ctrip/upgrade/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.ctrip.upgrade
 
 import android.os.Bundle
+import com.example.plugin.asr.AsrPlugin
 import io.flutter.app.FlutterActivity
 import io.flutter.plugins.GeneratedPluginRegistrant
 
@@ -8,5 +9,10 @@ class MainActivity: FlutterActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         GeneratedPluginRegistrant.registerWith(this)
+        registerSelfPlugin();
+    }
+
+    fun registerSelfPlugin(){
+        AsrPlugin.registerWith(registrarFor("com.example.plugin.asr.AsrPlugin"))
     }
 }
